760cd426d5bf4d3028d620d6ad6d9c1dcadcab32
fi.x64 <hohoangphia@gmail.com>
2022-08-10 11:58:14 +0700

    Add them lint-satged

--- src/index.js
+++ src/index.js
@@ -10,18 +10,24 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP logger
 // app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars.engine({ 
-  extname: '.hbs',
-  defaultLayout: 'main' }));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        defaultLayout: 'main',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -29,5 +35,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
